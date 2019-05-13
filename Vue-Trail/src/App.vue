d2fc08701ed82afb91c11a464b3628c7a7b1397a
Vinicius_Andrade <viniciusdeandrade04@gmail.com>
2019-05-12 23:16:35 -0300

    aula 5 vue

--- Vue-Trail/src/App.vue
+++ Vue-Trail/src/App.vue
@@ -1,30 +1,77 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
 
-    <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+    <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="Filtre pelo título">
+    
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      titulo: "Alurapic",
-      fotos: []
-    };
-  },
-  created() {
-    let promise = this.$http.get("http://localhost:3000/v1/fotos")
-      .then(res => res.json())
-      .then(fotos => this.fotos = fotos, ex => console.log("Erro interno"));
-  },
-};
+  import Painel from './components/shared/painel/Painel.vue';
+  
+  export default {
+    components: {
+      'meu-painel' : Painel
+    },
+    data() {
+      return {
+        titulo: "Alurapic",
+        fotos: [],
+        filtro: ''
+      };
+    },
+    created() {
+      let promise = this.$http
+        .get("http://localhost:3000/v1/fotos")
+        .then(res => res.json())
+        .then(fotos => (this.fotos = fotos), ex => console.log("Erro interno"));
+    },
+
+    computed: {
+      fotosComFiltro(){
+        if(this.filtro){
+          let reg = new RegExp(this.filtro.trim(),'i');
+          return this.fotos.filter(foto => reg.test(foto.titulo));
+        }
+        else{
+          return this.fotos;
+        }
+      }
+    },
+  };
 </script>
 
 <style>
+.corpo {
+  font-family: Arial, Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
+
+.centralizado {
+  text-align: center;
+}
+
+.lista-fotos {
+  list-style: none;
+}
+.lista-fotos-item {
+  display: inline-block;
+}
+
+.imagem-responsiva {
+    width: 100%;
+  }
+.filtro {
+  display: block;
+  width: 100%;
+}
 </style>
